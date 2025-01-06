a7fc02d27321857a72539d75b241c064d4a17dd3
Theodore R. Smith <theodore@phpexperts.pro>
2025-01-06 17:01:20 -0600

    Added Laravel migrations + DB Builder + API Key Manager and creator.

--- .framework/boot.php
+++ .framework/boot.php
@@ -1,8 +1,40 @@
 <?php
 
 use Dotenv\Dotenv;
+use Illuminate\Container\Container;
+use Illuminate\Database\Capsule\Manager;
+use Illuminate\Database\Migrations\DatabaseMigrationRepository;
+use Illuminate\Database\Migrations\Migrator;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\Facade;
 use Minicli\App;
+use Minicli\Command\CommandCall;
 use PHPExperts\ConsolePainter\ConsolePainter;
+use PHPExperts\MiniApiBase\ApiKeyManager;
+
+/** @var Manager $capsule */
+require __DIR__ . '/../database/config.php';
+
+$container = new Container;
+
+// Set up the event dispatcher
+$container->singleton('events', function() {
+    return new Dispatcher($this);
+});
+
+
+$container->instance('db', $capsule->getDatabaseManager());
+//$container->instance('db.schema', $capsule->getDatabaseManager()->getSchemaGrammar());
+
+// Bind 'db.schema' so Schema facade can resolve the schema builder
+$container->singleton('db.schema', function ($app) {
+    return $app->make('db')->connection()->getSchemaBuilder();
+});
+
+// Set the container instance to Facade
+Facade::setFacadeApplication($container);
+
 
 $app = new App([
     'app_path' => [
@@ -58,3 +90,62 @@ $validateEnv = function () use ($p) {
 };
 
 $validateEnv();
+
+
+
+$app->registerCommand('migrate', function () {
+    global $capsule;
+
+    $repository = new DatabaseMigrationRepository($capsule->getDatabaseManager(), 'migrations');
+    if (!$repository->repositoryExists()) {
+        $repository->createRepository();
+    }
+
+    $migrator = new Migrator($repository, $capsule->getDatabaseManager(), new Filesystem());
+    $migrationKeys = $migrator->run(__DIR__ . '/../database/migrations');
+
+    foreach ($migrationKeys as $filepath) {
+        $migrationKey = basename($filepath, '.php');
+        dump("Successfully ran the migration $migrationKey.");
+    }
+
+}, '<option>', 'Runs database migrations.');
+
+$app->registerCommand('migrate:rollback', function () {
+    global $capsule;
+
+    $repository = new DatabaseMigrationRepository($capsule->getDatabaseManager(), 'migrations');
+    if (!$repository->repositoryExists()) {
+        $repository->createRepository();
+    }
+
+    $migrator = new Migrator($repository, $capsule->getDatabaseManager(), new Filesystem());
+    $migrationKeys = $migrator->rollback(__DIR__ . '/../database/migrations');
+
+    foreach ($migrationKeys as $filepath) {
+        $migrationKey = basename($filepath, '.php');
+        dump("Successfully rolled back the migration $migrationKey.");
+    }
+
+}, '', 'Rolls back one migration at a time.');
+
+
+$app->registerCommand('api_key:issue', function (CommandCall $cli) use ($p) {
+    $argv = $cli->getRawArgs();
+    if (count($argv) < 3) {
+        echo $p->bold()->red("ERROR: ")->text('Missing the "client" CLI argument.') . "\n";
+        exit(1);
+    }
+
+    $client = $argv[2];
+
+    $keyMaster = new ApiKeyManager();
+    $apiKey = $keyMaster->createApiKey($client);
+
+    echo $p->bold("       Here is your API Key:") . "\n";
+    echo "\n";
+    echo $p->bold()->yellow("            $apiKey\n");
+    echo "\n";
+    echo $p->bold()->red("      Store it now, because it will never be reshown.") . "\n";
+
+}, '[client]', 'Issues a new API key for a client.');
